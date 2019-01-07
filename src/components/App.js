e305e27575d0834946550640e44f5d424379328f
mikeborowy <mikeborowy@gmail.com>
2019-01-07 23:19:58 +0100

    added css grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {unsplash} from '../api/index';
 import SearchBar from './SearchBar';
-
+import ImageList from './ImageList';
 
 class App extends Component {
 
@@ -23,6 +23,7 @@ class App extends Component {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmitSearch={this.onSubmitSearch}/>
+        <ImageList images={this.state.images}/>
         Found: {this.state.images.length} images
       </div>
     );
